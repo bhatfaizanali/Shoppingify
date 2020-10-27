7a058cb4dcc9566c6f16586ceca8b3493a1d19c1
Akash Gupta <aka1gpt@gmail.com>
2020-10-27 20:51:18 +0530

    Created list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Flex } from "@chakra-ui/core";
+
+import List from './List.js'
 
 class App extends Component {
   state = {};
   render() {
     return (
       <Router>
-        <div className="App">Hello User!</div>
+        <Flex className='App'>
+          <List />
+        </Flex>
       </Router>)
   }
 }
